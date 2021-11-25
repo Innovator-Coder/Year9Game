fcd4456f54825fa4301296ef8359ef3275486cfd
Innovator-Coder <malek.elhaddad@ucc.on.ca>
2021-11-25 09:16:22 -0500

    Colour Castle art

--- Executor.py
+++ Executor.py
@@ -2,6 +2,7 @@ import StoryLibrary as story
 import time 
 import termcolor
 
+
 greeting = input("Hey it's Jabari! I'm going to send you off on a little journey! Are you excited?\n")
 
 
@@ -14,40 +15,42 @@ time.sleep(1)
 
 print("We have 3 epic games! Firstly... ")
 
-dome1 ="                      .---.                  .---."
-dome2 ="                      |  .'                  |  .'"
-dome3 ="                       |-'                    |-'"
-dome4 ="                      |                      |"
-dome5 ="                    .---.                  .---."
-dome6 ="                   (:::::)________________(:::::)"
-dome7 ="                   / \/ \/ \/ \/ \/ \/ \/ \/ \/ \\"
-dome8 ="       __         /(_)(_)(_)(_)(_)(_)(_)(_)(_)(_)\         __"
-dome9 ="      (  ) _.-.  /''''''''''''''''''''''''''''''''\       (  ) _.-."
-dome10="       ||-|    `-._                                \       ||-|    `."
-dome11="       || |        `.                               \      || |      `."
-dome12="       ||-|_.-`-._   `.                              \     ||-|_.-`.   `."
+
+
+dome1 ="                      .---.                  .---.                        "
+dome2 ="                      |  .'                  |  .'                        "
+dome3 ="                       |-'                    |-'                         "
+dome4 ="                      |                      |                            "
+dome5 ="                    .---.                  .---.                          "
+dome6 ="                   (:::::)________________(:::::)                         "
+dome7 ="                   / \/ \/ \/ \/ \/ \/ \/ \/ \/ \\                        "
+dome8 ="       __         /(_)(_)(_)(_)(_)(_)(_)(_)(_)(_)\         __             "
+dome9 ="      (  ) _.-.  /''''''''''''''''''''''''''''''''\       (  ) _.-.       "
+dome10="       ||-|    `-._                                \       ||-|    `.     "
+dome11="       || |        `.                               \      || |      `.   "
+dome12="       ||-|_.-`-._   `.                              \     ||-|_.-`.   `. "
 dome13="       ||    /  .'`-. .'                              \    ||     .'`-. .'"
-dome14="       ||   /.-'  _.-'                                 \   ||  .-'  _.-'"
-dome15="       ||  /`._.-'                                      \  || `._.-'"
-dome16="       || /                                              \ ||"
-dome17="      //\\\\                                                //\\\\"
-dome18="     //  \\\\                                              //  \\\\"
-dome19="    // /\ \\\\                                            // /\ \\\\"
-dome20="   // /  \ \\\\                                          // /  \ \\\\"
-dome21="  // / /\ \ \\\\                                        // / /\ \ \\\\"
-dome22=" /_|_|/__\_\_\\\\                                      /_|_|/__\|_|_\\"
-dome23="/ _   _   _   _\____________________________________/  _   _   _   \\"
-dome24="|| |_| |_| |_| |------------------------------------|_| |_| |_| |_||"
-dome25="|______________|[=][=][=][=][=][=][=][=][=][=][=][=]|______________|"
-dome26="|--------------|____________________________________|--------------|"
-dome27="|   _______    |------------------------------------|    _______   |"
-dome28="|  |   _   |   |     |           .-.           /    |   |   _   |  |"
-dome29="|  |.'* *'.|   |     \     _..--('[|)-..__     |    |   |.'* *'.|  |"
-dome30="|  |'. * .'|   |      |_.-'   .-'/'\\'-.   '-._/     |   |'. * .'|  |"
-dome31="|  '. )_( .'   |      /   _.-' .'   '. '-._   \     |   '. )_( .'  |"
-dome32="|    '._.'     |     /_.-' _.-'       '-._ '-._\    |     '._.'    |"
-dome33="|______________|     |_.--'               '--._|    |______________|"
-dome34="|-.   .--.   .-|    /'                         '\ ' |-.   .--.   .-|"
+dome14="       ||   /.-'  _.-'                                 \   ||  .-'  _.-'  "
+dome15="       ||  /`._.-'                                      \  || `._.-'      "
+dome16="       || /                                              \ ||             "
+dome17="      //\\\\                                                //\\\\        "
+dome18="     //  \\\\                                              //  \\\\       "
+dome19="    // /\ \\\\                                            // /\ \\\\      "
+dome20="   // /  \ \\\\                                          // /  \ \\\\     "
+dome21="  // / /\ \ \\\\                                        // / /\ \ \\\\    "
+dome22=" /_|_|/__\_\_\\\\                                      /_|_|/__\|_|_\\    "
+dome23="/ _   _   _   _\____________________________________/  _   _   _   \\     "
+dome24="|| |_| |_| |_| |------------------------------------|_| |_| |_| |_||      "
+dome25="|______________|[=][=][=][=][=][=][=][=][=][=][=][=]|______________|      "
+dome26="|--------------|____________________________________|--------------|      "
+dome27="|   _______    |------------------------------------|    _______   |      "
+dome28="|  |   _   |   |     |           .-.           /    |   |   _   |  |      "
+dome29="|  |.'* *'.|   |     \     _..--('[|)-..__     |    |   |.'* *'.|  |      "
+dome30="|  |'. * .'|   |      |_.-'   .-'/'\\'-.   '-._/    |   |'. * .'|  |      "
+dome31="|  '. )_( .'   |      /   _.-' .'   '. '-._   \     |   '. )_( .'  |      "
+dome32="|    '._.'     |     /_.-' _.-'       '-._ '-._\    |     '._.'    |      "
+dome33="|______________|     |_.--'               '--._|    |______________|      "
+dome34="|-.   .--.   .-|    /'                         '\ ' |-.   .--.   .-|      "
 dome35="|  \ /    \ /  |  | |                          .' | |  \ /    \ /  |"
 dome36="| * v *  * v * | || '.                         | || | * v *  * v * |"
 dome37="|   *      *   | || ||                         | || |   *      *   |"
@@ -65,6 +68,113 @@ dome48="|              | `:\                            /:` |              |"
 dome49="|              | ' .`                          `. ' |              |"
 dome50="|              |--`                              `--|              |"
 dome51="`--...____...--'                                    `--...____...--'"
+
+colourdome1=termcolor.colored(dome1,'red',"on_grey")
+colourdome2=termcolor.colored(dome2,'yellow',"on_grey")
+colourdome3=termcolor.colored(dome3,'green',"on_grey")
+colourdome4=termcolor.colored(dome4,'blue',"on_grey")
+colourdome5=termcolor.colored(dome5,'cyan',"on_grey")
+colourdome6=termcolor.colored(dome6,'magenta',"on_grey")
+colourdome7=termcolor.colored(dome7,'red',"on_grey")
+colourdome8=termcolor.colored(dome8,'yellow',"on_grey")
+colourdome9=termcolor.colored(dome9,'green',"on_grey")
+colourdome10=termcolor.colored(dome10,'blue',"on_grey")
+colourdome11=termcolor.colored(dome11,'magenta',"on_grey")
+colourdome12=termcolor.colored(dome12,'cyan',"on_grey")
+colourdome13=termcolor.colored(dome13,'red',"on_grey")
+colourdome14=termcolor.colored(dome14,'green',"on_grey")
+colourdome15=termcolor.colored(dome15,'yellow',"on_grey")
+colourdome16=termcolor.colored(dome16,'blue',"on_grey")
+colourdome17=termcolor.colored(dome17,'magenta',"on_grey")
+colourdome18=termcolor.colored(dome18,'cyan',"on_grey")
+colourdome19=termcolor.colored(dome19,'red',"on_grey")
+colourdome20=termcolor.colored(dome20,'yellow',"on_grey")
+colourdome21=termcolor.colored(dome21,'blue',"on_grey")
+colourdome22=termcolor.colored(dome22,'yellow',"on_grey")
+colourdome23=termcolor.colored(dome23,'green',"on_grey")
+colourdome24=termcolor.colored(dome24,'magenta',"on_grey")
+colourdome25=termcolor.colored(dome25,'cyan',"on_grey")
+colourdome26=termcolor.colored(dome26,'red',"on_grey")
+colourdome27=termcolor.colored(dome27,'yellow',"on_grey")
+colourdome28=termcolor.colored(dome28,'blue',"on_grey")
+colourdome29=termcolor.colored(dome29,'green',"on_grey")
+colourdome30=termcolor.colored(dome30,'cyan',"on_grey")
+colourdome31=termcolor.colored(dome31,'magenta',"on_grey")
+colourdome32=termcolor.colored(dome32,'red',"on_grey")
+colourdome33=termcolor.colored(dome33,'yellow',"on_grey")
+colourdome34=termcolor.colored(dome34,'blue',"on_grey")
+colourdome35=termcolor.colored(dome35,'red',"on_grey")
+colourdome36=termcolor.colored(dome36,'green',"on_grey")
+colourdome37=termcolor.colored(dome37,'yellow',"on_grey")
+colourdome38=termcolor.colored(dome38,'blue',"on_grey")
+colourdome39=termcolor.colored(dome39,'cyan',"on_grey")
+colourdome40=termcolor.colored(dome40,'magenta',"on_grey")
+colourdome41=termcolor.colored(dome41,'red',"on_grey")
+colourdome42=termcolor.colored(dome42,'yellow',"on_grey")
+colourdome43=termcolor.colored(dome43,'green',"on_grey")
+colourdome44=termcolor.colored(dome44,'blue',"on_grey")
+colourdome45=termcolor.colored(dome45,'magenta',"on_grey")
+colourdome46=termcolor.colored(dome46,'cyan',"on_grey")
+colourdome47=termcolor.colored(dome47,'red',"on_grey")
+colourdome48=termcolor.colored(dome48,'yellow',"on_grey")
+colourdome49=termcolor.colored(dome49,'green',"on_grey")
+colourdome50=termcolor.colored(dome50,'blue',"on_grey")
+colourdome51=termcolor.colored(dome51,'magenta',"on_grey")
+
+
+print(colourdome1)
+print(colourdome2)
+print(colourdome3)
+print(colourdome4)
+print(colourdome5)
+print(colourdome6)
+print(colourdome7)
+print(colourdome8)
+print(colourdome9)
+print(colourdome10)
+print(colourdome11)
+print(colourdome12)
+print(colourdome13)
+print(colourdome14)
+print(colourdome15)
+print(colourdome16)
+print(colourdome17)
+print(colourdome18)
+print(colourdome19)
+print(colourdome20)
+print(colourdome21)
+print(colourdome22)
+print(colourdome23)
+print(colourdome24)
+print(colourdome25)
+print(colourdome26)
+print(colourdome27)
+print(colourdome28)
+print(colourdome29)
+print(colourdome30)
+print(colourdome31)
+print(colourdome32)
+print(colourdome33)
+print(colourdome34)
+print(colourdome35)
+print(colourdome36)
+print(colourdome37)
+print(colourdome38)
+print(colourdome39)
+print(colourdome40)
+print(colourdome41)
+print(colourdome42)
+print(colourdome43)
+print(colourdome44)
+print(colourdome45)
+print(colourdome46)
+print(colourdome47)
+print(colourdome48)
+print(colourdome49)
+print(colourdome50)
+print(colourdome51)
+
+
 
 #story.randomStory()
 # print(dome1)
